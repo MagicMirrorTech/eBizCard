11e826230225534794644475574727cdb9557fdd
James Keck <bestdealdenver@Jamess-iMac.local>
2020-03-08 22:10:09 -0600

    fixed contacts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,65 @@
+// Get screen size
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var agent = navigator.userAgent;
+var platform = navigator.platform;
 
 
-	function goToLink(link, name)	 {
-		if (link.includes('@')) {
-			var arg = email + '?' + subj + '&' + body;
-			var url = 'mailto:' + arg;
-		}
-		else  if (link.includes('http')){
-			var url = link;
-		}
-		else {die}
 
-		echo(url);
-		return window.location.href = url;
+
+function goToV(){
+	if (w < 500 && platform.indexof('os') < -1 ) {
+		window.open('../dan/dan_h.ics');
 	}
+	else {
+		window.open('../dan/dan_h.vcf');
+	}
+}
+
+function goToW(){
+	 window.open("https://www.bestdealdenver.com");
+}
+
+function goToE(){
+	//Edit
+	var name = 'Dan';
+	var email = 'danielbestdeal@gmail.com';
+
+	//Do not edit
+	var subj = 'subject=Introduction';
+	var body = 'body=Hi ' + name + ', Look forward to connecting.';
+	var url = email+'?'+subj+'&'+body;
+
+	window.open("mailto:"+url);
+}
+
+function goToA(){
+	window.open('https://goo.gl/maps/Mn7i59tMc1LN1BvR9');
+}
 
 
-// Get email and website divs
-	var email = document.getElementById('emailDiv');
-	var web = document.getElementById('webDiv');
 
-	email.onclick = goToLink('danielbestdeal@gmail.com', 'Dan');
+
+//
+// 	function goToLink(link, name)	 {
+// 		if (link.includes('@')) {
+// 			var arg = email + '?' + subj + '&' + body;
+// 			var url = 'mailto:' + arg;
+// 		}
+// 		else  if (link.includes('http')){
+// 			var url = link;
+// 		}
+// 		else {die}
+//
+// 		echo(url);
+// 		return window.location.href = url;
+// 	}
+//
+//
+// // Get email and website divs
+// 	var email = document.getElementById('emailDiv');
+// 	var web = document.getElementById('webDiv');
+//
+// 	email.onclick = goToLink('danielbestdeal@gmail.com', 'Dan');
