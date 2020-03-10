85ff4e00063c20153a7b1c1faf328de032e1e023
James Keck <bestdealdenver@Jamess-iMac.local>
2020-03-10 07:28:17 -0600

    added .env

---
+++ send_sms.js
@@ -0,0 +1,15 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+14692085882',
+		 mediaUrl: ['https://ebizcard.netlify.com/dan/dan_h.vcf'],
+     to: '+13368800967'
+   })
+  .then(message => console.log(message.sid));
